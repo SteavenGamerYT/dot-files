8aca436f93007b97e7f0a029979a7d0eaef9284e
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2023-04-28 22:51:44 +0300

    Update flatpak-font.sh

--- flatpak-font.sh
+++ flatpak-font.sh
@@ -1,1 +1,2 @@
 flatpak list --app --columns=application,runtime|grep org.gnome.Platform|awk -F '\t' '{print "flatpak run --command=fc-cache " $1 " -f -v "}'|xargs -I % echo "%"|sh
+flatpak run --command=fc-cache org.onlyoffice.desktopeditors -f -v 
