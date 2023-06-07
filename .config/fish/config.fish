f6c9e166d828c6415a27c8822442e944cd8559cc
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2023-06-08 00:09:09 +0300

    ah

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,9 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    # Commands to run in interactive sessions can go here.
+### ADDING TO THE PATH
+# First line removes the path; second line sets it.  Without the first line,
+# your path gets massive and fish becomes very slow.
+set -e fish_user_paths
+set -U fish_user_paths $HOME/.bin  $HOME/.local/bin $HOME/Applications /var/lib/flatpak/exports/bin/ $fish_user_paths
     eval "$(starship init fish)"
 end
