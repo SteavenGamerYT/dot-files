d03bf54238812b03fdca04219fa2feca7e1eaeac
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2023-07-27 17:14:19 +0300

    Update topbar.py

--- vim-nano/topbar.py
+++ vim-nano/topbar.py
@@ -4,7 +4,7 @@ import sys
 while True:
     _, cols = os.popen('stty size', 'r').read().split()
     cols = int(cols)
-    prefix = '\r\033[7m  GNU nano 6.6.6'
+    prefix = '\r\033[7m  Nano + VIM = Vimao | NeoVim'
     suffix = ''
     output = prefix + ' '*(cols - (len(prefix)-5 + len(suffix))) + suffix
     sys.stdout.write(output)
