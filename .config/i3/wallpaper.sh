9e15965e7ee72caf379fb23c94c81a2d421a462b
SteavenGamerYT <omarhanykasban706@gmail.com>
2023-11-09 18:45:40 +0200

    fixs

--- .config/i3/wallpaper.sh
+++ .config/i3/wallpaper.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-img=(`find ~/Nextcloud/HDD/Pictures/SteavenGamerYT/ -name '*' -exec file {} \; | grep -o -P '^.+: \w+ image' | cut -d':' -f1`)
+img=(`find ~/Documents/GitHub/nord-background/ -name '*' -exec file {} \; | grep -o -P '^.+: \w+ image' | cut -d':' -f1`)
 while true
 do
    feh --bg-scale --randomize --no-fehbg "${img[$RANDOM % ${#img[@]} ]}"
