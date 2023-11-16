9d05fc4cbf55f6ae7b756a20d7616cc4114e5e9c
Omar Hany Kasban <omarhanykasban706@gmail.com>
2023-11-16 08:34:37 +0200

    better display

---
+++ .config/i3/setup-monitors-moderm.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+sleep 5
+
+# Check if X server is running
+if ! command -v xrandr &> /dev/null; then
+    echo "Error: xrandr command not found. Make sure you are running this script in a graphical environment."
+    exit 1
+fi
+
+# Get the display variable
+display=:0
+
+# Check if it's a Dell G15 5510
+if sudo lshw | grep -q "Dell G15 5510"; then
+    DISPLAY=$display xrandr --output DP-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-0 --off --output eDP-1-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-1-1 --off --output HDMI-1-1 --off --output DP-1-2 --off --output HDMI-1-2 --off
+
+# Check if it's an HP 350 G1
+elif sudo lshw | grep -q "HP 350 G1"; then
+    echo "ah"
+else
+    echo "Unknown laptop model"
+fi
