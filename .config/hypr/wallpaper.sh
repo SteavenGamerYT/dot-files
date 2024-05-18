64d710ff40adbb12460fe4fa3d4fd07c416584a7
SteavenGamerYT <omarhanykasban706@gmail.com>
2024-05-18 12:28:10 +0300

    hyprland fixs

--- .config/hypr/wallpaper.sh
+++ .config/hypr/wallpaper.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-img=(`find ~/Pictures/SteavenGamerYT/ -name '*' -exec file {} \; | grep -o -P '^.+: \w+ image' | cut -d':' -f1`)
+img=(`find ~/Pictures/nord-background/ -name '*' -exec file {} \; | grep -o -P '^.+: \w+ image' | cut -d':' -f1`)
 while true
 do
    swaybg -m fill -i "${img[$RANDOM % ${#img[@]} ]}"
