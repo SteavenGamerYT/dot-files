b8b7c961e9e48d1d0d8b01afd9341cef5c992b03
SteavenGamerYT <omarhanykasban706@gmail.com>
2024-06-05 21:06:25 +0300

    a

--- .config/i3/lockscreen.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/bash
-xidlehook --not-when-fullscreen --not-when-audio --timer 300 'betterlockscreen -l'
--- .config/i3/touchpad-mouse-config.sh
+++ .config/i3/touchpad-mouse-config.sh
@@ -1,26 +1,40 @@
 #!/bin/bash
+# Function to disable mouse acceleration
+disable_mouse_acceleration() {
+    local device_id
+    device_id=$(xinput list --id-only "$1")
+    if [ -n "$device_id" ]; then
+        if xinput list-props "$device_id" | grep -q "libinput Accel Profile Enabled"; then
+            xinput set-prop "$device_id" "libinput Accel Profile Enabled" 0, 1
+        fi
+        if xinput list-props "$device_id" | grep -q "libinput Accel Speed"; then
+            xinput set-prop "$device_id" "libinput Accel Speed" 0
+        fi
+    fi
+}
 
-# Find the touchpad device ID
-touchpad_id=$(xinput list | grep "Touchpad" | grep -oP 'id=\K\d+')
+# Function to enable tapping and natural scrolling for touchpads
+enable_touchpad_settings() {
+    local device_id
+    device_id=$(xinput list --id-only "$1")
+    if [ -n "$device_id" ]; then
+        if xinput list-props "$device_id" | grep -q "libinput Tapping Enabled"; then
+            xinput set-prop "$device_id" "libinput Tapping Enabled" 1
+        fi
+        if xinput list-props "$device_id" | grep -q "libinput Natural Scrolling Enabled"; then
+            xinput set-prop "$device_id" "libinput Natural Scrolling Enabled" 1
+        fi
+    fi
+}
 
-# Check if touchpad_id is not empty
-if [ -n "$touchpad_id" ]; then
-    # Enable tapping and natural scrolling for the touchpad
-    xinput --set-prop "$touchpad_id" "libinput Tapping Enabled" 1
-    xinput --set-prop "$touchpad_id" "libinput Natural Scrolling Enabled" 1
-    # Uncomment the line below if you want to set Natural Scrolling as default
-    # xinput --set-prop "$touchpad_id" "libinput Natural Scrolling Enabled Default" 1
-fi
+# Get the list of all input devices
+devices=$(xinput list --name-only)
 
-# Set mouse acceleration for the SINOWEALTH Wired Gaming Mouse
-mouse_id=$(xinput list | grep "SINOWEALTH Wired Gaming Mouse" | grep -oP 'id=\K\d+')
-
-# Check if mouse_id is not empty
-if [ -n "$mouse_id" ]; then
-    xinput --set-prop "$mouse_id" "libinput Accel Profile Enabled" 0, 1
-fi
-
-# Set mouse acceleration for all pointer devices
-for id in $(xinput list | grep "pointer" | cut -d '=' -f 2 | cut -f 1); do
-    xinput --set-prop "$id" 'libinput Accel Profile Enabled' 0, 1
-done
+# Loop through each device and apply the appropriate settings
+while IFS= read -r device; do
+    if [[ "$device" == *"mouse"* || "$device" == *"Mouse"* ]]; then
+        disable_mouse_acceleration "$device"
+    elif [[ "$device" == *"touchpad"* || "$device" == *"Touchpad"* ]]; then
+        enable_touchpad_settings "$device"
+    fi
+done <<< "$devices"
