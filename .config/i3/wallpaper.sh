05a181933f31843d300f960af897847a957ce9bc
SteavenGamerYT <omarhanykasban706@gmail.com>
2024-07-11 00:41:28 +0300

    i3 uh

--- .config/i3/wallpaper.sh
+++ .config/i3/wallpaper.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-img=(`find ~/Pictures/nord-background -name '*' -exec file {} \; | grep -o -P '^.+: \w+ image' | cut -d':' -f1`)
+img=(`find /mnt/hdd/Pictures/SteavenGamerYT2 -name '*' -exec file {} \; | grep -o -P '^.+: \w+ image' | cut -d':' -f1`)
 while true
 do
    feh --bg-scale --randomize --no-fehbg "${img[$RANDOM % ${#img[@]} ]}"
