f74b347c27a59ff5b380da7d95421e31973729b4
Omar Hany Kasban <omarhanykasban706@gmail.com>
2024-09-19 15:51:32 +0300

    Update toggle-keyboard-layout.sh

--- DotFiles/.config/waybar/sway/scripts/toggle-keyboard-layout.sh
+++ DotFiles/.config/waybar/sway/scripts/toggle-keyboard-layout.sh
@@ -3,15 +3,17 @@
 # Run hyprctl command to get keyboard information,
 # extract active_keymap, convert to lowercase,
 # and get the first two characters
-keyboard_lang=$(hyprctl devices -j | jq -r '.keyboards[] | .active_keymap' | sed -n '6p' | cut -c1-2 | tr 'A-Z' 'a-z')
+keyboard_lang=$(~/.config/waybar/sway/scripts/get-lang.sh)
 
 # Check if the keyboard language is "en"
-if [ "$keyboard_lang" = "en" ]; then
-    hyprctl keyword input:kb_layout ara,us
-    hyprctl keyword input:kb_options grp:alt_shift_toggle
-elif [ "$keyboard_lang" = "ar" ]; then
-    hyprctl keyword input:kb_layout us,ara
-    hyprctl keyword input:kb_options grp:alt_shift_toggle
+if [ "$keyboard_lang" = "us" ]; then
+    swaymsg input type:keyboard xkb_layout "eg,us"
+    swaymsg input type:keyboard xkb_options "grp:alt_shift_toggle"
+    swaymsg input type:keyboard xkb_numlock enabled
+elif [ "$keyboard_lang" = "eg" ]; then
+    swaymsg input type:keyboard xkb_layout "us,eg"
+    swaymsg input type:keyboard xkb_options "grp:alt_shift_toggle"
+    swaymsg input type:keyboard xkb_numlock enabled
 else
     echo "Unknown keyboard language: $keyboard_lang"
 fi
