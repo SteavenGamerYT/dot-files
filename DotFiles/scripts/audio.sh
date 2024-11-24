3f1f192a1b1e9c7a492d4c78ad188c8e6f5d1bc5
SteavenGamerYT <omarhanykasban706@gmail.com>
2024-11-24 16:47:12 +0200

    improved audio script
    improved audio script

--- DotFiles/scripts/audio.sh
+++ DotFiles/scripts/audio.sh
@@ -9,7 +9,7 @@ pactl set-card-profile alsa_card.pci-0000_01_00.1 pro-audio
 # H848 Wireless Headset
 pactl set-card-profile alsa_card.usb-XiiSound_Technology_Corporation_H848_Wireless_headset-00 output:iec958-stereo+input:mono-fallback
 
-# H848 USB Gamign Headset
+# H848 USB Headset
 pactl set-card-profile alsa_card.usb-XiiSound_Technology_Corporation_H848_USB_Gaming_Headset-00 output:iec958-stereo+input:mono-fallback
 
 # Change Headset pcm to 100%
@@ -24,6 +24,7 @@ pactl set-default-sink alsa_output.usb-XiiSound_Technology_Corporation_H848_Wire
 # Set H848 Wireless Headset as default mic
 pactl set-default-source alsa_input.usb-XiiSound_Technology_Corporation_H848_Wireless_headset-00.mono-fallback
 
+# Audio
 # undo first
 pw-link -d alsa_output.usb-XiiSound_Technology_Corporation_H848_Wireless_headset-00.iec958-stereo alsa_output.pci-0000_01_00.1.pro-output-7
 pw-link -d alsa_output.usb-XiiSound_Technology_Corporation_H848_Wireless_headset-00.iec958-stereo alsa_output.usb-XiiSound_Technology_Corporation_H848_USB_Gaming_Headset-00.iec958-stereo
@@ -34,3 +35,12 @@ sleep 2
 pw-link alsa_output.usb-XiiSound_Technology_Corporation_H848_Wireless_headset-00.iec958-stereo alsa_output.pci-0000_01_00.1.pro-output-7
 pw-link alsa_output.usb-XiiSound_Technology_Corporation_H848_Wireless_headset-00.iec958-stereo alsa_output.usb-XiiSound_Technology_Corporation_H848_USB_Gaming_Headset-00.iec958-stereo
 
+
+# Mic
+# undo first
+pactl list short modules | grep "source=alsa_input.usb-XiiSound_Technology_Corporation_H848_Wireless_headset-00.mono-fallback" | awk '{print $1}' | xargs -I {} pactl unload-module {}
+
+sleep 2
+
+# do it now
+pactl load-module module-loopback source=alsa_input.usb-XiiSound_Technology_Corporation_H848_Wireless_headset-00.mono-fallback sink=alsa_output.pci-0000_07_00.6.analog-stereo
