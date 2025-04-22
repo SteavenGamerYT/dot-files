c51bd5bf8bfad8ce6f0e1cedc72ba8d0d456768e
Omar Hany Kasban <omarhanykasban706@gmail.com>
2025-04-22 22:12:34 +0200

    fixs

--- DotFiles/scripts/audio3.sh
+++ DotFiles/scripts/audio3.sh
@@ -4,6 +4,10 @@
 pactl set-card-profile alsa_card.usb-XiiSound_Technology_Corporation_H848_Wireless_headset-00 output:iec958-stereo+input:mono-fallback
 pactl set-card-profile alsa_card.usb-XiiSound_Technology_Corporation_H848_USB_Gaming_Headset-00 output:iec958-stereo+input:mono-fallback
 
+# Change Gpu Audio profiles to the proper ones
+pactl set-card-profile alsa_card.pci-0000_08_00.1 pro-audio
+pactl set-card-profile alsa_card.pci-0000_03_00.1 pro-audio
+
 # Change Headset pcm to 100%
 amixer -c headset set PCM 100%
 amixer -c headset set PCM,1 100%
