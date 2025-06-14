5e35b913302e210ce98c287e4fe7986db18443be
SteavenGamerYT <omarhanykasban706@gmail.com>
2025-06-14 22:08:10 +0300

    a

--- DotFiles/.config/waybar/Omar-PC/sway/scripts/cpu-fan-cycle.sh
+++ DotFiles/.config/waybar/Omar-PC/sway/scripts/cpu-fan-cycle.sh
@@ -4,6 +4,7 @@ STATE_FILE="$HOME/.cache/waybar_cpu_fan_cycle"
 CPU_SCRIPT="$HOME/.config/waybar/Omar-PC/sway/scripts/cpu-usage.sh"
 FAN_SCRIPT="$HOME/.config/waybar/Omar-PC/sway/scripts/fans.sh"
 TEMP_SCRIPT="$HOME/.config/waybar/Omar-PC/sway/scripts/cpu-temp.sh"
+WATT_SCRIPT="$HOME/.config/waybar/Omar-PC/sway/scripts/cpu-watts.sh"  # <-- Make sure this exists!
 
 # Create state file if missing
 [ -f "$STATE_FILE" ] || echo "fan" > "$STATE_FILE"
@@ -13,19 +14,24 @@ STATE=$(<"$STATE_FILE")
 CPU=$(bash "$CPU_SCRIPT")
 FAN=$(bash "$FAN_SCRIPT")
 TEMP=$(bash "$TEMP_SCRIPT")
+WATT=$(bash "$WATT_SCRIPT")
 
 case "$STATE" in
   fan)
     TEXT="$FAN"
-    TOOLTIP=" $CPU\n$TEMP"
+    TOOLTIP=" $CPU\n$TEMP\n$WATT"
     ;;
   temp)
     TEXT="$TEMP"
-    TOOLTIP=" $CPU\n$FAN"
+    TOOLTIP=" $CPU\n$FAN\n$WATT"
+    ;;
+  watts)
+    TEXT="$WATT"
+    TOOLTIP=" $CPU\n$TEMP\n$FAN"
     ;;
   usage|*)
     TEXT=" $CPU"
-    TOOLTIP="$TEMP\n$FAN"
+    TOOLTIP="$TEMP\n$WATT\n$FAN"
     ;;
 esac
 
--- DotFiles/.config/waybar/Omar-PC/sway/scripts/cycle-cpu-fan.sh
+++ DotFiles/.config/waybar/Omar-PC/sway/scripts/cycle-cpu-fan.sh
@@ -6,7 +6,8 @@ STATE_FILE="$HOME/.cache/waybar_cpu_fan_cycle"
 STATE=$(<"$STATE_FILE")
 
 case "$STATE" in
-  fan)   echo "temp" > "$STATE_FILE" ;;
-  temp)  echo "usage" > "$STATE_FILE" ;;
+  fan)    echo "temp" > "$STATE_FILE" ;;
+  temp)   echo "watts" > "$STATE_FILE" ;;
+  watts)  echo "usage" > "$STATE_FILE" ;;
   usage|*) echo "fan" > "$STATE_FILE" ;;
 esac
