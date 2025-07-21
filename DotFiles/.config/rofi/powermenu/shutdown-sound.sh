4c66e425bab8747fec70ec23c66220f0784e2563
SteavenGamerYT <omarhanykasban706@gmail.com>
2025-07-21 16:28:24 +0300

    imporvements for flatpak
    imporvements for flatpak

--- DotFiles/.config/rofi/powermenu/shutdown-sound.sh
+++ DotFiles/.config/rofi/powermenu/shutdown-sound.sh
@@ -2,8 +2,34 @@
 
 mpv --config-dir=/dev/null --no-video --no-resume-playback ~/Audio/mp3/Sounds/sonic-shutdown.m4a
 
-beep -f 880 -l 100   # A5
-beep -f 1046 -l 100  # C6
-beep -f 1318 -l 300  # E6
-beep -f 988  -l 200  # B5
-beep -f 880  -l 200  # A5
+# Sonic the Hedgehog - Act Clear Theme (extended & expressive with beep)
+
+beep -f 1318 -l 160   # E6
+sleep 0.06
+beep -f 1318 -l 160   # E6
+sleep 0.07
+beep -f 1567 -l 160   # G#6
+sleep 0.05
+beep -f 1760 -l 200   # A6
+sleep 0.1
+
+beep -f 1396 -l 120   # F#6
+sleep 0.05
+beep -f 1175 -l 120   # D6
+sleep 0.05
+beep -f 1318 -l 160   # E6
+sleep 0.07
+beep -f 988  -l 130   # B5
+sleep 0.04
+beep -f 880  -l 130   # A5
+sleep 0.04
+
+beep -f 1046 -l 160   # C6
+sleep 0.06
+beep -f 784  -l 130   # G5
+sleep 0.04
+beep -f 880  -l 170   # A5
+sleep 0.08
+beep -f 698  -l 130   # F5
+sleep 0.04
+beep -f 659  -l 400   # E5
